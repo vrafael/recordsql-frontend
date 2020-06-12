7e181bddf80d1790a9bc23198a0fefa427301662
coldrain96 <Justtryagain961@gmail.com>
2020-06-12 14:37:35 +0500

    added new routes: '/', '/types', '/records'

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,9 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: '/types', component: () => import('pages/Types.vue') },
+      { path: '/records', component: () => import('pages/Records.vue') },
     ]
   }
 ]
