61c917fc381eb1f74563c5f8914730bd74737162
coldrain96 <Justtryagain961@gmail.com>
2020-06-18 12:07:28 +0500

    added new routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,7 +8,15 @@ const routes = [
       { path: '/types', component: () => import('pages/Types.vue') },
       { path: '/records', component: () => import('pages/Records.vue') },
     ]
-  }
+  },
+  {
+    path: '/records',
+    component: () => import('layouts/HomeLayout.vue'),
+    children: [
+      { path: 'fields', component: () => import('components/Records/Fields/Fields') },
+      { path: 'relations', component: () => import('components/Records/Relations/Relations') },
+    ]
+  },
 ]
 
 // Always leave this as last one
