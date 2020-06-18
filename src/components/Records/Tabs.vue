d3598d7656b7e2ffd92721c3a2b983582bb7976f
coldrain96 <Justtryagain961@gmail.com>
2020-06-18 12:11:03 +0500

    created Records/Tabs component

---
+++ src/components/Records/Tabs.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="record__tabs">
+    <div class="row">
+      <q-tabs
+        v-model="tab"
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-route-tab exact to="/records/fields" name="fields" label="Fields"/>
+        <q-route-tab exact to="/records/relations" name="relations" label="Relations"/>
+      </q-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tab: 'mails'
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
