8ec764bd7972664858b76b0e6d726dccceb8a564
coldrain96 <Justtryagain961@gmail.com>
2020-06-18 12:11:33 +0500

    created Records/Relations component

---
+++ src/components/Records/Relations/Relations.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page class="flex">
+    <q-row class="full-width" style="max-width: 1500px">
+      <div class="col-12 q-mx-auto">
+        <q-card class="q-mx-xs">
+
+          <q-card-section>
+            <Header/>
+            <Tabs/>
+          </q-card-section>
+          <!--        <q-page-container>-->
+          <!--          <router-view/>-->
+          <!--        </q-page-container>-->
+        </q-card>
+      </div>
+    </q-row>
+  </q-page>
+</template>
+
+<script>
+  import Tabs from 'components/Records/Tabs'
+  import Header from 'components/Records/Header'
+
+  export default {
+    components: {
+      Tabs,
+      Header,
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
