9a21a521955d540245d99dedad13862dcd366568
coldrain96 <Justtryagain961@gmail.com>
2020-06-19 17:07:22 +0500

    second version of rFieldLink component implementation

--- src/components/Records/Fields/rFieldLink.vue
+++ src/components/Records/Fields/rFieldLink.vue
@@ -4,18 +4,25 @@
       {{ name }}
     </div>
     <div class="col-9">
-      <q-input class="text-body1" v-model="defaultValue" outlined readonly>
-        <q-btn-dropdown class="q-my-xs" color="primary" label="Select">
-          <q-list>
-            <q-item clickable v-close-popup>
-              <q-item-section style="display:flex; flex-flow: row; align-items: center">
-                <q-icon :name="icon" size="sm"></q-icon>
-                <q-item-label class="text-subtitle2">{{`${name}s ...`}}</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-btn-dropdown>
-      </q-input>
+<!--      <q-input class="text-body1" v-model="defaultValue" outlined readonly>-->
+<!--        <q-btn-dropdown class="q-my-xs" color="primary" label="Select">-->
+<!--          <q-list>-->
+<!--            <q-item clickable v-close-popup>-->
+<!--              <q-item-section style="display:flex; flex-flow: row; align-items: center">-->
+<!--                <q-icon :name="icon" size="sm"></q-icon>-->
+<!--                <q-item-label class="text-subtitle2">{{`${name}s ...`}}</q-item-label>-->
+<!--              </q-item-section>-->
+<!--            </q-item>-->
+<!--          </q-list>-->
+<!--        </q-btn-dropdown>-->
+<!--      </q-input>-->
+      <q-select
+        v-model="modelMultiple"
+        multiple
+        :options="options"
+        use-chips
+        stack-label
+      />
     </div>
   </div>
 </template>
@@ -26,6 +33,12 @@
       name: String,
       icon: String,
       defaultValue: String,
-    }
+    },
+    data: () => ({
+      modelMultiple: ['2rsoft'],
+      options: [
+        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+      ]
+    })
   }
 </script>
