aa0169be5c76955dce0fb1bbb2da24d3a12117a2
coldrain96 <Justtryagain961@gmail.com>
2020-06-19 17:47:18 +0500

    realised rFieldBool component

--- src/components/Records/Fields/rFieldBool.vue
+++ src/components/Records/Fields/rFieldBool.vue
@@ -1,10 +1,24 @@
 <template>
-
+  <div class="row q-my-md">
+    <div
+      class="text-h6"
+      style="display: flex;align-items: center;">
+      {{ questionText }}
+    </div>
+    <q-checkbox
+      toggle-indeterminate
+      indeterminate-value="maybe"
+      v-model="theModel2"/>
+  </div>
 </template>
 
 <script>
+  export default {
+    props: {
+      questionText: String,
+    },
+    data: () => ({
+      theModel2: null
+    }),
+  }
 </script>
-
-<style scoped>
-
-</style>
