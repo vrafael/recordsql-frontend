c376019bcf811f1c4a494e2d0596f71b9c50feb1
coldrain96 <Justtryagain961@gmail.com>
2020-06-22 19:35:41 +0500

    typo fix in rFieldDatetime component

--- src/components/Records/Fields/rFieldDatetime.vue
+++ src/components/Records/Fields/rFieldDatetime.vue
@@ -52,8 +52,8 @@
                   v-model="creatingTime"
                   format24h>
                   <div class="row items-center justify-end q-gutter-sm">
-                    <q-btn label="Cancel" color="primary" flat v-close-popup/>
                     <q-btn label="OK" color="primary" flat @click="save" v-close-popup/>
+                    <q-btn label="Cancel" color="primary" flat v-close-popup/>
                   </div>
                 </q-time>
               </q-popup-proxy>
