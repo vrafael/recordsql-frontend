b05f3ca5e4bc0e9c2e8129d19766f7565de17c62
coldrain96 <Justtryagain961@gmail.com>
2020-06-22 17:29:46 +0500

    realised rFieldInt component

--- src/components/Records/Fields/rFieldInt.vue
+++ src/components/Records/Fields/rFieldInt.vue
@@ -1,8 +1,26 @@
 <template>
-
+  <div class="row q-my-md">
+    <div class="col-3 text-h6" style="display: flex;align-items: center;">
+      Enter the number
+    </div>
+    <div class="col-9">
+      <q-input
+        v-model="integer"
+        mask="#"
+        fill-mask="0"
+        reverse-fill-mask
+        input-class="text-right text-h6"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+  export default {
+    data: () => ({
+      integer: 0,
+    }),
+  }
 </script>
 
 <style scoped>
