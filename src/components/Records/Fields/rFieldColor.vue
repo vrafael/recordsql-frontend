271e3973dec626174c17a3b8cb126a0fe334a4a5
coldrain96 <Justtryagain961@gmail.com>
2020-06-25 19:09:07 +0300

    code cleanup & new features in rFieldColor component

--- src/components/Records/Fields/rFieldColor.vue
+++ src/components/Records/Fields/rFieldColor.vue
@@ -6,36 +6,37 @@
     <div class="col-9">
       <q-input
         mask="\#NNNNNN"
-        v-model="color.backgroundColor"
+        v-model="inputColor"
         :rules="[ val => val.length === 7 || 'Please use 6 characters']"
         class="my-input"
       >
-        <template v-slot:append>
-          <q-icon name="colorize" class="cursor-pointer">
-            <q-popup-proxy transition-show="scale" transition-hide="scale">
-              <q-card>
-                <q-color v-model="color.backgroundColor"/>
-                <div class="row items-center justify-end q-gutter-sm">
-                  <q-btn
-                    label="OK"
-                    color="primary"
-                    flat
-                    v-close-popup/>
-                  <q-btn
-                    label="Cancel"
-                    color="primary"
-                    flat
-                    v-close-popup/>
-                </div>
-              </q-card>
-
-            </q-popup-proxy>
-          </q-icon>
-        </template>
-
-        <template v-slot:prepend>
-          <div :style="color" class="color-helper"/>
-        </template>
+        <q-icon
+          @click="applyColorToPicker"
+          slot="append"
+          name="colorize"
+          class="cursor-pointer">
+          <q-popup-proxy
+            transition-show="scale"
+            transition-hide="scale">
+            <q-card>
+              <q-color v-model="pickerColor"/>
+              <div class="row items-center justify-end">
+                <q-btn
+                  @click="applyColorToInput"
+                  label="OK"
+                  color="primary"
+                  flat
+                  v-close-popup/>
+                <q-btn
+                  label="Cancel"
+                  color="primary"
+                  flat
+                  v-close-popup/>
+              </div>
+            </q-card>
+          </q-popup-proxy>
+        </q-icon>
+        <div slot="prepend" :style="helperColor.style" class="color-helper"/>
       </q-input>
     </div>
   </div>
@@ -44,11 +45,34 @@
 <script>
   export default {
     data: () => ({
-      color: {
-        backgroundColor: '#000000',
-        width: '20px',
-        height: '20px',
+      inputColor: '',
+      helperColor: {
+        style: {
+          backgroundColor: '',
+          width: '20px',
+          height: '20px',
+        }
       },
+      pickerColor: '',
     }),
+    watch: {
+      inputColor: function (val) {
+        if (val.length === 4 || val.length === 7) {
+          this.helperColor.style.backgroundColor = val;
+        }
+      },
+    },
+    mounted() {
+      this.helperColor.style.backgroundColor = this.inputColor;
+    },
+    methods: {
+      applyColorToInput() {
+        this.inputColor = this.pickerColor
+      },
+      applyColorToPicker() {
+        this.pickerColor = this.inputColor
+      },
+
+    }
   }
 </script>
