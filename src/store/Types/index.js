a5270c76cfd66e64c46b24108419c74cb7a877c7
coldrain96 <Justtryagain961@gmail.com>
2020-07-09 15:43:36 +0300

    feat(store/Types): import new functions in index

--- src/store/Types/index.js
+++ src/store/Types/index.js
@@ -1,8 +1,12 @@
 import state from './state'
 import getters from './getters'
+import mutations from './mutations'
+import actions from './actions'
 
 export default {
   namespaced: true,
   getters,
-  state
+  state,
+  mutations,
+  actions
 }
