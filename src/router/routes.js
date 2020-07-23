9c8f5f5fd2e8bdef76b08d4ae2db6be81396c179
coldrain96 <justtryagain961@gmail.com>
2020-07-23 13:57:26 +0300

    fix(routes): delete children's

--- src/router/routes.js
+++ src/router/routes.js
@@ -20,20 +20,7 @@ const routes = [{
     component: () => import('pages/rRecord.vue'),
     props: (route) => ({
       id: parseInt(route.params.id)
-    }),
-    children: [{
-      path: 'fields',
-      name: 'recordfields',
-      component: () => import('components/Record/rFields'),
-      props: true
-    },
-    {
-      path: 'relations',
-      name: 'recordrelations',
-      component: () => import('components/Record/rRelations'),
-      props: true
-    }
-    ]
+    })
   }
   ]
 }]
