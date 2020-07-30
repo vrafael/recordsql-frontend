1669968c18b9e80760ecd109058dbd6df9038e46
coldrain96 <justtryagain961@gmail.com>
2020-07-30 12:20:13 +0300

    feat(helpers): create notify service

---
+++ src/common/service.notify.js
@@ -0,0 +1,27 @@
+import { Notify } from 'quasar'
+
+export default function showNotifyByServerResponse ({ errorMessage, errorCode, displayTimeMS }) {
+  try {
+    let notifyType
+
+    if (errorCode >= 100 && errorCode <= 105) {
+      notifyType = 'information'
+    } else if (errorCode >= 200 && errorCode <= 226) {
+      notifyType = 'notification'
+    } else if (errorCode >= 400 && errorCode <= 499) {
+      notifyType = 'clientError'
+    } else if (errorCode >= 500 && errorCode <= 510) {
+      notifyType = 'serverError'
+    } else {
+      notifyType = 'defaultError'
+    }
+
+    Notify.create({
+      type: notifyType,
+      message: `${errorMessage}`,
+      timeout: displayTimeMS
+    })
+  } catch (e) {
+    throw new Error(`Can not create notify: ${e}`)
+  }
+}
