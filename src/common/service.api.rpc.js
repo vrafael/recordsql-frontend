d3d3d4322d2854e85aaf61226c7a5d8e6d36b205
coldrain96 <justtryagain961@gmail.com>
2020-07-30 12:25:56 +0300

    feat(helpers): add notify function to service.api.rpc

--- src/common/service.api.rpc.js
+++ src/common/service.api.rpc.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import showNotifyByServerResponse from './service.notify'
 
 export default async function fetchApiRPC (method, params) {
   return axios.post('/api/rpc/', {
@@ -6,22 +7,38 @@ export default async function fetchApiRPC (method, params) {
     params
   }).then(response => {
     if (response.status !== 200) {
+      const errorMessage = response.data.error.message
+      const errorCode = response.data.error.code
+      showNotifyByServerResponse({
+        errorMessage: errorMessage,
+        errorCode: errorCode,
+        displayTimeMS: 5000
+      })
       console.groupCollapsed('/api/rpc internal service error')
       console.error(response)
       console.info(response)
       console.groupEnd()
     } else if (response.data.error) {
-      const {
-        error
-      } = response.data
+      const { error } = response.data
+      const errorMessage = response.data.error.message
+      const errorCode = response.data.error.code
+      showNotifyByServerResponse({
+        errorMessage: errorMessage,
+        errorCode: errorCode,
+        displayTimeMS: 5000
+      })
       console.groupCollapsed(`/api/rpc request error with code:${error.code}`)
-      console.error(error.message)
+      console.error(error)
       console.info(response)
       console.groupEnd()
     } else {
       return response.data.result
     }
   }).catch(error => {
+    showNotifyByServerResponse({
+      errorMessage: error,
+      displayTimeMS: 5000
+    })
     console.groupCollapsed('/api/rpc connection error')
     console.error(error)
     console.groupEnd()
