fc72200272ecef008355c3c22fa7279c1a724d65
coldrain96 <justtryagain961@gmail.com>
2020-07-31 14:55:09 +0300

    fix(components/find/rfind): layout max-height fixes

--- src/components/Find/rFind.vue
+++ src/components/Find/rFind.vue
@@ -1,5 +1,6 @@
 <template>
   <q-splitter
+    style="max-height: 85vh"
     v-model="splitter"
     horizontal
     unit="%"
@@ -154,7 +155,7 @@ export default {
 
 <style lang="sass">
 .my-sticky-dynamic
-  height: 600px
+  height: 100%
 
   .q-table__top,
   .q-table__bottom,
