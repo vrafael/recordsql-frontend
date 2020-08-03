ad03f174059697fd652e9f8cd2dc2c01d16b856b
coldrain96 <justtryagain961@gmail.com>
2020-08-03 17:10:10 +0300

    feat(components/find/filters): multiple improvements
    rFilterFloat, rFilterInt, rFilterString

--- src/components/Find/Filters/rFilterFloat.vue
+++ src/components/Find/Filters/rFilterFloat.vue
@@ -1,37 +1,40 @@
 <template>
   <r-filter
     :field="field"
-    :enable.sync="enable"
+    :filter="filter"
   >
     <q-input
       class="col-4"
       type="number"
-      v-model="valueFrom"
-      :disable="!enable"
+      :value="filter.ValueFrom"
+      :disable="!filter.Enable"
       :rules="intInputRules"
+      @change="event => updateValueFrom(event.target.value)"
       outlined
       dense
       ref="inputFrom"
-      clearable
+      :clearable="filter.ValueFrom !== filterOrigin.ValueFrom"
       @clear="resetFrom"
     />
     <q-space />
     <q-input
       class="col-4"
       type="number"
-      v-model="valueTo"
-      :disable="!enable"
+      :value="filter.ValueTo"
+      :disable="!filter.Enable"
       :rules="intInputRules"
+      @change="event => updateValueTo(event.target.value)"
       outlined
       dense
       ref="inputTo"
-      clearable
+      :clearable="filter.ValueTo !== filterOrigin.ValueTo"
       @clear="resetTo"
     />
   </r-filter>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 import rFilter from './rFilter'
 
 export default {
@@ -40,6 +43,14 @@ export default {
   },
   props: {
     field: {
+      type: Object,
+      required: true
+    },
+    filter: {
+      type: Object,
+      required: true
+    },
+    filterOrigin: {
       type: Object,
       required: true
     }
@@ -51,15 +62,44 @@ export default {
     ]
   }),
   methods: {
+    ...mapActions([
+      'FILTER_STATE_UPDATE_FIELD'
+    ]),
     resetFrom () {
       setTimeout(() => {
         this.$refs.inputFrom.resetValidation()
       })
+      const filter = { ...this.filter }
+      filter.ValueFrom = this.filterOrigin.ValueFrom
+      const obj = { [`${this.field.Tag}`]: filter }
+      this.FILTER_STATE_UPDATE_FIELD(obj)
+    },
+    updateValueFrom (eventValue) {
+      const filter = { ...this.filter }
+      filter.ValueFrom = Number(eventValue)
+      if (filter.ValueTo && filter.ValueFrom > filter.ValueTo) {
+        filter.ValueTo = filter.ValueFrom
+      }
+      const obj = { [`${this.field.Tag}`]: filter }
+      this.FILTER_STATE_UPDATE_FIELD(obj)
     },
     resetTo () {
       setTimeout(() => {
         this.$refs.inputTo.resetValidation()
       })
+      const filter = { ...this.filter }
+      filter.ValueTo = this.filterOrigin.ValueTo
+      const obj = { [`${this.field.Tag}`]: filter }
+      this.FILTER_STATE_UPDATE_FIELD(obj)
+    },
+    updateValueTo (eventValue) {
+      const filter = { ...this.filter }
+      filter.ValueTo = Number(eventValue)
+      if (filter.ValueFrom && filter.ValueTo < filter.ValueFrom) {
+        filter.ValueFrom = filter.ValueTo
+      }
+      const obj = { [`${this.field.Tag}`]: filter }
+      this.FILTER_STATE_UPDATE_FIELD(obj)
     }
   }
 }
