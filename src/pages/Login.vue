0011eab5fc488ef33cc060cdcacd38922a721fd1
Kamil Bikmetov <56452785+coldrain96@users.noreply.github.com>
2020-08-20 19:22:52 +0300

    feat(pages/Login): create Login page

---
+++ src/pages/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-page-container>
+      <login-form />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import LoginForm from 'src/components/Login/LoginForm'
+
+export default {
+  components: {
+    LoginForm
+  }
+}
+</script>
